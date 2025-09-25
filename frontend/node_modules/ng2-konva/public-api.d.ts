deca24a971ca19f1b46f181ce302476730a9183d
Giaeulate <giaeulate@gmail.com>
2025-09-25 16:54:53 -0400

    bg

--- frontend/node_modules/ng2-konva/public-api.d.ts
+++
@@ -1,4 +0,0 @@
-export { CoreShapeComponent } from './lib/components/core-shape.component';
-export { StageComponent } from './lib/components/stage.component';
-export { KonvaComponent } from './lib/interfaces/ko-component.interface';
-export { NgKonvaEventObject } from './lib/interfaces/ngKonvaEventObject';
